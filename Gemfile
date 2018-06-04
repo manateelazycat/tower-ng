c84692f6e26d1a103ae884f7669f196b035c5195
Andy Stewart <lazycat.manatee@gmail.com>
2018-06-04 10:35:49 +0800

    Build user model.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+gem 'bcrypt'
 gem 'bootstrap-will_paginate', '1.0.0'
 gem 'bootstrap-sass'
 # Use sqlite3 as the database for Active Record
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            length: {maximum: 100}
+
+  before_save {self.email = email.downcase}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+end
