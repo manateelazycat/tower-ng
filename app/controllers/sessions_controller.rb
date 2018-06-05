c60d731e52767f6f723de1acfdfc3e6e9531e9f7
Andy Stewart <lazycat.manatee@gmail.com>
2018-06-05 10:07:33 +0800

    Add cookie control.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remeber_me] == '1' ? remeber(user) : forget(user)
       redirect_to teams_path
     else
       flash.now[:danger] = "错误的邮箱或密码"
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_022029) do
+ActiveRecord::Schema.define(version: 2018_06_05_005235) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_022029) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
