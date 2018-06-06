bb0d1a8aae9edb2e655f8a194d1ea5f52d383faa
Andy Stewart <lazycat.manatee@gmail.com>
2018-06-06 16:53:46 +0800

    Use 'browser' gem for browse device detector.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  
+  before_action :detect_browse_device
+  
+  private
+  def detect_browse_device
+    request.variant = :phone if browser.device.mobile?
+  end
 end
