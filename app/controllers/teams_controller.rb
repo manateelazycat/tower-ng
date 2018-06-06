c0920a2a33ff93639ccd17d59b9a1862f6b332a8
Andy Stewart <lazycat.manatee@gmail.com>
2018-06-06 17:37:11 +0800

    Use controller action controller teams page sign in status.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,6 +10,9 @@ class TeamsController < ApplicationController
   end
   
   def show
-    
+    # Redirect to root path if no user log in.
+    if !logged_in?
+      redirect_to root_path
+    end
   end
 end
