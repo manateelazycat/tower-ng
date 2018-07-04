b91d9af5ad69170eab6710543aaa7b22c805d0a7
Andy Stewart <lazycat.manatee@gmail.com>
2018-07-04 15:49:08 +0800

    Remove type attr from submit button, daemon bug.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,14 @@ class ProjectsController < ApplicationController
   end
 
   def new
-  
+  end
+
+  def create
+    puts "********"
+    puts params[:project][:name]
+    puts params[:project][:summary]
+    puts "********"
+
+    jump_to_team_homepage(current_user)
   end
 end
