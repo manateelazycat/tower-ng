feda4397bf0b42b8f8fd734330b8cf5134f70041
Andy Stewart <lazycat.manatee@gmail.com>
2018-07-05 11:31:51 +0800

    Add some comment.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,17 +18,19 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    # Get team.
     team = Team.find_by_hashid(params[:team_id])
 
+    # Create project.
     project = team.projects.create(
       team_id: params[:team_id],
       name: params[:project][:name],
       summary: params[:project][:summary],
       icon: @@glyphicon_list.sample,
     )
-
     project.save
 
+    # Redirect to project home page after create new project.
     jump_to_team_homepage(current_user)
   end
 end
