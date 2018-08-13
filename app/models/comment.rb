dc72229511b5a8924a927604240e8b37f13bd0a5
Andy Stewart <lazycat.manatee@gmail.com>
2018-08-13 18:54:50 +0800

    Finish comment date format.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,20 @@
+# coding: utf-8
 class Comment < ApplicationRecord
   include Hashid::Rails
 
   belongs_to :mission
+
+  def get_format_date
+    seconds = DateTime.now.to_time.to_i - self.created_at.to_time.to_i
+
+    if seconds < 60
+      return "刚刚"
+    elsif seconds < 3600
+      return "%i分钟前" % [seconds / 60]
+    elsif seconds < 3600 * 24
+      return "%i小时前" % [seconds / 3600]
+    else
+      return self.created_at.strftime("%Y-%m-%d")
+    end
+  end
 end
