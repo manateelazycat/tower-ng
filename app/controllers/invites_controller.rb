9f7c2cdf64c001743067c4bd7c17ee7d84927119
Andy Stewart <lazycat.manatee@gmail.com>
2018-08-25 12:01:12 +0800

    Use JS filter invite messages, rails/form can't work with list input data.

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,11 @@
+# coding: utf-8
 class InvitesController < ApplicationController
+  def new
+  end
+
+  def create
+    params[:members].values().reverse.uniq{|m| m[0]}.reverse.each do |member|
+      print("**** ", member[0], " ", member[1], "\n")
+    end
+  end
 end
--- app/javascript/controllers/invite_controller.js
+++ app/javascript/controllers/invite_controller.js
@@ -15,6 +15,69 @@ export default class extends Controller {
     clickSubmitButton(event) {
 	event.preventDefault()
 
-	console.log("*********")
+	var members = []
+	var inviteEmailItems = $(".invite-email-item")
+
+	$.each(inviteEmailItems, function(index, item) {
+	    var email = $(item).find(".invite-edit-input").val().trim()
+
+	    if (email.length > 0) {
+		members.push([email, $(item).find("select").val()])
+	    }
+	})
+
+	if (members.length > 0) {
+	    var url = $(location).attr('href')
+	    var urlParams = url.split("/")
+
+	    $.ajax({
+		type: "POST",
+		url: "/teams/" + urlParams[4] + "/invites",
+		data: {
+		    members: members,
+		},
+		success: function(result) {
+
+		}
+	    })
+	} else {
+            this.updateTooltip(this.createTooltip("请输入邮箱"), inviteEmailItems.first())
+	}
+    }
+
+    createTooltip(text) {
+        var tooltip
+
+        // Fade in tooltip element if it exists.
+        if ($(".input-tooltip").length) {
+            tooltip = $(".input-tooltip")
+            tooltip.text(text)
+            tooltip.fadeIn(0)
+        }
+        // Otherwise create tooltip element.
+        else {
+            tooltip = $("<div />")
+            tooltip.attr({class: "input-tooltip"});
+            tooltip.text(text)
+            $("body").append(tooltip)
+        }
+
+        return tooltip
+    }
+
+    updateTooltip(tooltip, input) {
+        var scrollOffset = window.scrollY
+        var tooltipHideTimeout = 3000
+        var tooltipHideDuration = 400
+        var tooltipArrowWidth = 20
+
+        // Adjust tooltip coordinate.
+        tooltip.css({
+            top: scrollOffset + input[0].getBoundingClientRect().top,
+            left: input[0].getBoundingClientRect().left - tooltip.outerWidth(true) - tooltipArrowWidth
+        })
+
+        // Hide tooltip after duration.
+        tooltip.delay(tooltipHideTimeout).fadeOut(tooltipHideDuration)
     }
 }
