96df743f338929ad4bb2868ea390c257be7bd5fd
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-03 17:45:13 +0800

    Make resource join only use 'new' and 'create' method.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
-  resources :joins
+  resources :joins, only: [:new, :create]
 
   resources :projects do
     resources :mission_lists
