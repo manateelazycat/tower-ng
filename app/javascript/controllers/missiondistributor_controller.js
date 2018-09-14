6f64f5f4cbf6c8d6da644e222ba74004aa87f143
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-14 15:34:41 +0800

    Delete unused js funciton.

--- app/javascript/controllers/missiondistributor_controller.js
+++ app/javascript/controllers/missiondistributor_controller.js
@@ -143,10 +143,6 @@ export default class extends Controller {
 	missionMemberInput.data("userid", $(currentTarget).find(".mission-member-userid").text().trim())
     }
 
-    clickMissionListDistributorButton(event) {
-	
-    }
-
     clickDistributorButton(event) {
         event.preventDefault()
 
