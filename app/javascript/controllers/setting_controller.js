37bf3395b1bf30269fa0c6976effef6fc6008c62
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-21 13:43:57 +0800

    I missing targets after static.

--- app/javascript/controllers/setting_controller.js
+++ app/javascript/controllers/setting_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static = [ "submit" ]
+    static targets = [ "submit" ]
 
     finishUpload(event) {
 	this.submitTarget.click()
