53002452c2719c01c82e68c7e8afe01fcb093c8e
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-26 20:36:13 +0800

    Finish notification menu.

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -6,9 +6,13 @@ export default class extends Controller {
  	if (exclueMenuElements(event, [".top-header-menu", ".top-header-menu-button"])) {
 	    $(".top-header-menu").hide()
 	}
+
+     	if (exclueMenuElements(event, [".top-header-bell", ".notification-menu"])) {
+	    $(".notification-menu").hide()
+	}
     }
 
-    popMenu(event) {
+    popMainMenu(event) {
 	event.preventDefault()
 
         var currentTarget = event.currentTarget
@@ -20,6 +24,23 @@ export default class extends Controller {
 	topHeaderMenu
 	    .css({
 		left: currentRect.left + currentRect.width - topHeaderMenu.width() + menuTriangleWidth * 2,
+		top: currentRect.bottom + menuTriangleWidth
+	    })
+	    .show()
+    }
+
+    popNotificationMenu(event) {
+	event.preventDefault()
+
+        var currentTarget = event.currentTarget
+	var currentRect = currentTarget.getBoundingClientRect()
+
+	var menuTriangleWidth = 10
+	var notificationMenu = $(".notification-menu")
+
+	notificationMenu
+	    .css({
+		left: currentRect.left + currentRect.width - notificationMenu.width() + menuTriangleWidth * 10,
 		top: currentRect.bottom + menuTriangleWidth
 	    })
 	    .show()
