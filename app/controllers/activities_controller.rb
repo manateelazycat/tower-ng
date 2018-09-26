a3c117f7a8b61edf29bb35873566a02f90ab27cb
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-27 00:55:12 +0800

    Don't show in activity if log_info is nil (mission deleted by user)

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -13,23 +13,26 @@ class ActivitiesController < ApplicationController
 
     activity_items.each do |activity_item|
       log_info = activity_item.log_info
-      log_day = log_info[:created_time].strftime("%Y-%m-%d")
 
-      if current_day != log_day
-        current_day = log_day
-        current_project_name = log_info[:project_name]
-        @activities.push(log_type: "day_line",
-                         log_day: log_day == today ? "今天" : log_day,
-                         mission_project_hashid: log_info[:mission_project_hashid],
-                         project_name: log_info[:project_name])
-      elsif current_project_name != log_info[:project_name]
-        current_project_name = log_info[:project_name]
-        @activities.push(log_type: "project_line",
-                         mission_project_hashid: log_info[:mission_project_hashid],
-                         project_name: log_info[:project_name])
+      if log_info
+        log_day = log_info[:created_time].strftime("%Y-%m-%d")
+
+        if current_day != log_day
+          current_day = log_day
+          current_project_name = log_info[:project_name]
+          @activities.push(log_type: "day_line",
+                           log_day: log_day == today ? "今天" : log_day,
+                           mission_project_hashid: log_info[:mission_project_hashid],
+                           project_name: log_info[:project_name])
+        elsif current_project_name != log_info[:project_name]
+          current_project_name = log_info[:project_name]
+          @activities.push(log_type: "project_line",
+                           mission_project_hashid: log_info[:mission_project_hashid],
+                           project_name: log_info[:project_name])
+        end
+
+        @activities.push(log_info)
       end
-
-      @activities.push(log_info)
     end
 
     @activities
